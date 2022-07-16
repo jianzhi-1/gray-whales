9e41725e20cb952e14875987785b4b11fced187c
jaewookim5830 <109431467+jaewookim5830@users.noreply.github.com>
2022-07-16 13:42:13 -0700

    updated adr

--- adr.py
+++ adr.py
@@ -13,10 +13,8 @@ def adr_signal(cs_trade_price_list, adr_trade_price_list):
         return [adr_mean, cs_mean]
     return []
 
-def adr_strategy(vale_info, valbz_trade):
+def adr_strategy(vale_trade_price_list, valbz_trade_price_list):
     # Retrieve the list of stock prices
-    vale_trade_price_list = list(map(lambda x: x[0], vale_info))
-    valbz_trade_price_list = list(map(lambda x: x[0], valbz_trade))
     if len(vale_trade_price_list) >= 10 and len(valbz_trade_price_list) >= 10:
         vale = vale_trade_price_list[-10:]
         valbz = valbz_trade_price_list[-10:]
